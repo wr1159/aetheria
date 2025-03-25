665ee59d1e1dddd18958e754de66e3800ef79208
wr1159 <chuweiron18@gmail.com>
2025-03-25 23:28:09 +0000

    Add TailwindCSS

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,11 +2,12 @@ import { dirname, resolve } from "node:path";
 import { fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react(), tailwindcss()],
     build: {
         rollupOptions: {
             input: {
