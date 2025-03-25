578365bc20fc24ceff70f91452aff39dcbc4f26c
wr1159 <chuweiron18@gmail.com>
2025-03-25 23:08:52 +0000

    Update vite config for multipage

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,4 +1,16 @@
-/** @type {import('vite').UserConfig} */
-export default {
-    base: "./"
-};
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
+import { defineConfig } from "vite";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig({
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, "index.html"),
+                mint: resolve(__dirname, "mint/index.html"),
+            },
+        },
+    },
+});
